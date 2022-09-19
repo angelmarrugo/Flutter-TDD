d6722279a73bdecd8a1cb3a22d0a96ed64d811e5
angelmarrugo <angel3087426>
2022-09-19 15:58:54 -0500

    :construction: [ADD] Local data source

--- lib/features/cat_detail/data/datasources/cat_local_data_source.dart
+++ lib/features/cat_detail/data/datasources/cat_local_data_source.dart
@@ -1,3 +1,9 @@
+import 'dart:convert';
+
+import 'package:app_cats/core/error/exception.dart';
+import 'package:app_cats/core/shared/constants/constants_storage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 import '/features/cat_detail/data/models/cat_dto.dart';
 
 /// Contracts
@@ -10,4 +16,35 @@ abstract class CatLocalDataSource {
 
   /// Saved in local storage data of cats
   Future<void> cacheCats(List<CatDTO> cats);
+}
+
+/// Implementation of contract
+class CatLocalDataSourceImpl implements CatLocalDataSource {
+  /// Constructor
+  CatLocalDataSourceImpl({required this.sharedPreferences});
+
+  /// Instance of [SharedPreferences]
+  final SharedPreferences sharedPreferences;
+
+  @override
+  Future<List<CatDTO>> getCats() {
+    final jsonString = sharedPreferences.getString(CACHED_CATS);
+    List<CatDTO> catsDTO = [];
+    if (jsonString != null) {
+      final jsonMap = json.decode(jsonString);
+      for (var item in jsonMap) {
+        catsDTO.add(CatDTO.fromJson(item));
+      }
+    } else {
+      throw CacheException();
+    }
+
+    return Future.value(catsDTO);
+  }
+
+  @override
+  Future<void> cacheCats(List<CatDTO> cats) {
+    // TODO: implement cacheCats
+    throw UnimplementedError();
+  }
 }
